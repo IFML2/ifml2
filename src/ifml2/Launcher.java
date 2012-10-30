d202688e85de6fd0a324efaccce475125a52f70f
realsonic <realsonic2@gmail.com>
2012-10-30 09:32:32 +0400

    logging system properties

--- src/ifml2/Launcher.java
+++ src/ifml2/Launcher.java
@@ -2,14 +2,25 @@ package ifml2;
 
 import ifml2.editor.gui.Editor;
 import ifml2.players.guiplayer.GUIPlayer;
+import org.apache.log4j.Logger;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 public class Launcher
 {
+    private static final Logger LOG = Logger.getLogger(Launcher.class);
+
     /**
      * @param args if first arg is 'player' or 'editor' then appropriate program will be launched, else player is launched with first parameter
      */
     public static void main(String[] args)
     {
+        // log system properties
+        StringWriter stringWriter = new StringWriter();
+        System.getProperties().list(new PrintWriter(stringWriter));
+        LOG.debug(String.format("System properties:\n%s", stringWriter.toString()));
+
         if(args.length > 0 && "player".equalsIgnoreCase(args[0]))
         {
             GUIPlayer.main(getOtherArgs(args));
