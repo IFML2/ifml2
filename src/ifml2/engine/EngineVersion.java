5513885abe8063cb15b20ab2a05e61db5fa6587c
realsonic <realsonic2@gmail.com>
2013-01-26 20:48:27 +0400

    д   ~   Улучшен вывод при осмотре предмета: для открываемых предметов теперь ставится подпись "(открыт)" или "(закрыт)" д   +   При открытии предмета выводится список содержащихся в нём предметов.

--- src/ifml2/engine/EngineVersion.java
+++ src/ifml2/engine/EngineVersion.java
@@ -2,5 +2,5 @@ package ifml2.engine;
 
 public class EngineVersion
 {
-    public static final String IFML_ENGINE_VERSION = "Прототип 10 выпуск 1";
+    public static final String IFML_ENGINE_VERSION = "Прототип 10 выпуск 2";
 }
