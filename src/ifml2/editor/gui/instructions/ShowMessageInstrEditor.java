bf581075960e547a283f8b89e0d01e07df02519c
realsonic <realsonic2@gmail.com>
2013-03-09 02:58:12 +0400

    +   Редактор инструкции "Вывести сообщение" доработан до актуального состояния.

--- src/ifml2/editor/gui/instructions/ShowMessageInstrEditor.java
+++ src/ifml2/editor/gui/instructions/ShowMessageInstrEditor.java
@@ -2,23 +2,33 @@ package ifml2.editor.gui.instructions;
 
 import ifml2.GUIUtils;
 import ifml2.vm.instructions.ShowMessageInstr;
+import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.text.MessageFormat;
+
+import static ifml2.vm.instructions.ShowMessageInstr.MessageTypeEnum.EXPRESSION;
+import static ifml2.vm.instructions.ShowMessageInstr.MessageTypeEnum.TEXT;
 
 public class ShowMessageInstrEditor extends JDialog
 {
-    public static final String SHOWMESSAGE_INSTREDITOR_TITLE = "Вывести сообщение";
+    private static final String SHOW_MESSAGE_INSTR_EDITOR_TITLE = "Вывести сообщение";
+    private static final String TYPE_ERROR = "Message type \"{0}\" is unknown";
     private JPanel contentPane;
     private JButton buttonOK;
     private JButton buttonCancel;
     private JTextArea messageText;
+    private JRadioButton textTypeRadio;
+    private JRadioButton exprTypeRadio;
+    private JCheckBox beginWithCapCheck;
+    private JCheckBox carriageReturnCheck;
     private boolean isOk = false;
 
-    public ShowMessageInstrEditor(Window owner, ShowMessageInstr instruction)
+    public ShowMessageInstrEditor(Window owner, @NotNull ShowMessageInstr instruction)
     {
-        super(owner, SHOWMESSAGE_INSTREDITOR_TITLE, ModalityType.DOCUMENT_MODAL);
+        super(owner, SHOW_MESSAGE_INSTR_EDITOR_TITLE, ModalityType.DOCUMENT_MODAL);
 
         setContentPane(contentPane);
         getRootPane().setDefaultButton(buttonOK);
@@ -60,7 +70,23 @@ public class ShowMessageInstrEditor extends JDialog
             }
         }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
 
-        setData(instruction);
+        // -- init form --
+        switch (instruction.getType())
+        {
+            case TEXT:
+                textTypeRadio.setSelected(true);
+                break;
+            case EXPRESSION:
+                exprTypeRadio.setSelected(true);
+                break;
+            default:
+                throw new InternalError(MessageFormat.format(TYPE_ERROR, instruction.getType()));
+        }
+
+        messageText.setText(instruction.getMessage());
+
+        beginWithCapCheck.setSelected(instruction.getBeginWithCap());
+        carriageReturnCheck.setSelected(instruction.getCarriageReturn());
     }
 
     private void onOK()
@@ -75,14 +101,25 @@ public class ShowMessageInstrEditor extends JDialog
         dispose();
     }
 
-    public void setData(ShowMessageInstr data)
-    {
-        messageText.setText(data.getMessage());
-    }
-
     public void getData(ShowMessageInstr data)
     {
+        if(textTypeRadio.isSelected())
+        {
+            data.setType(TEXT);
+        }
+        else if(exprTypeRadio.isSelected())
+        {
+            data.setType(EXPRESSION);
+        }
+        else
+        {
+            throw new InternalError("No type is selected!");
+        }
+
         data.setMessage(messageText.getText());
+
+        data.setBeginWithCap(beginWithCapCheck.isSelected());
+        data.setCarriageReturn(carriageReturnCheck.isSelected());
     }
 
     public boolean showDialog()
