7ad7492b19bc1f6d3da3dd8c57ff974a4cc437b4
realsonic <realsonic2@gmail.com>
2013-08-28 23:10:16 +0400

    ifml2-210289 Возможно создать предмет без словарной записи

--- src/ifml2/editor/gui/ItemEditor.java
+++ src/ifml2/editor/gui/ItemEditor.java
@@ -6,6 +6,7 @@ import ca.odell.glazedlists.swing.DefaultEventListModel;
 import ca.odell.glazedlists.swing.DefaultEventSelectionModel;
 import ifml2.GUIUtils;
 import ifml2.IFML2Exception;
+import ifml2.editor.DataNotValidException;
 import ifml2.om.*;
 import org.jetbrains.annotations.NotNull;
 
@@ -16,6 +17,7 @@ import java.awt.event.*;
 
 public class ItemEditor extends AbstractEditor<Item>
 {
+    private static final String ITEM_EDITOR_TITLE = "Предмет";
     private JPanel contentPane;
     private JButton buttonOK;
     private JButton buttonCancel;
@@ -32,43 +34,12 @@ public class ItemEditor extends AbstractEditor<Item>
     private JButton addHookButton;
     private JButton editHookButton;
     private JButton deleteHookButton;
-
-    private static final String ITEM_EDITOR_TITLE = "Предмет";
-
     private Story story = null;
     private boolean toGenerateId = false;
-
     // clones
     private EventList<Attribute> attributesClone = null;
     private WordLinks wordLinksClone = null;
     private EventList<Hook> hooksClone = null;
-
-    // updated actions
-    private final AbstractAction editHookAction = new AbstractAction("Редактировать...", GUIUtils.EDIT_ELEMENT_ICON)
-    {
-        @Override()
-        public void actionPerformed(ActionEvent e)
-        {
-            Hook selectedHook = (Hook) hooksList.getSelectedValue();
-            if (selectedHook != null)
-            {
-                editHook(selectedHook);
-            }
-        }
-    };
-    private final AbstractAction deleteHookAction = new AbstractAction("Удалить", GUIUtils.DEL_ELEMENT_ICON)
-    {
-        @Override
-        public void actionPerformed(ActionEvent e)
-        {
-            Hook selectedHook = (Hook) hooksList.getSelectedValue();
-            if (selectedHook != null && JOptionPane.showConfirmDialog(ItemEditor.this, "Вы действительно хотите удалить выбранный перехват?",
-                    "Удаление перехвата", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION)
-            {
-                hooksClone.remove(selectedHook);
-            }
-        }
-    };
 
     public ItemEditor(Window owner, @NotNull final Story story, @NotNull final Item item)
     {
@@ -83,7 +54,7 @@ public class ItemEditor extends AbstractEditor<Item>
             public void actionPerformed(ActionEvent e)
             {
                 ObjectAttributesEditor objectAttributesEditor = new ObjectAttributesEditor(ItemEditor.this, attributesClone, ItemEditor.this.story);
-                if(objectAttributesEditor.showDialog())
+                if (objectAttributesEditor.showDialog())
                 {
                     objectAttributesEditor.getData(attributesClone);
                 }
@@ -95,13 +66,13 @@ public class ItemEditor extends AbstractEditor<Item>
             public void actionPerformed(ActionEvent e)
             {
                 WordLinksEditor wordLinksEditor = new WordLinksEditor(ItemEditor.this, story.getDictionary(), wordLinksClone);
-                if(wordLinksEditor.showDialog())
+                if (wordLinksEditor.showDialog())
                 {
                     wordLinksEditor.getData(wordLinksClone);
                 }
             }
         });
-        
+
         // hooks
         addHookButton.setAction(new AbstractAction("Добавить...", GUIUtils.ADD_ELEMENT_ICON)
         {
@@ -109,24 +80,62 @@ public class ItemEditor extends AbstractEditor<Item>
             public void actionPerformed(ActionEvent e)
             {
                 Hook hook = new Hook();
-                if(editHook(hook))
+                if (editHook(hook))
                 {
                     hooksClone.add(hook);
                 }
             }
         });
-        editHookButton.setAction(editHookAction);
-        deleteHookButton.setAction(deleteHookAction);
-        hooksList.addListSelectionListener(new ListSelectionListener()
+
+        editHookButton.setAction(new AbstractAction("Редактировать...", GUIUtils.EDIT_ELEMENT_ICON)
         {
-            @Override
-            public void valueChanged(ListSelectionEvent e)
             {
-                UpdateHookActions();
+                setEnabled(false); // initially disabled
+                hooksList.addListSelectionListener(new ListSelectionListener()
+                {
+                    @Override
+                    public void valueChanged(ListSelectionEvent e)
+                    {
+                        setEnabled(!hooksList.isSelectionEmpty()); // dependent from selection
+                    }
+                });
+            }
+
+            @Override()
+            public void actionPerformed(ActionEvent e)
+            {
+                Hook selectedHook = (Hook) hooksList.getSelectedValue();
+                if (selectedHook != null)
+                {
+                    editHook(selectedHook);
+                }
             }
         });
-        
-        // --- set data with listeners ---
+        deleteHookButton.setAction(new AbstractAction("Удалить", GUIUtils.DEL_ELEMENT_ICON)
+        {
+            {
+                setEnabled(false); // initially disabled
+                hooksList.addListSelectionListener(new ListSelectionListener()
+                {
+                    @Override
+                    public void valueChanged(ListSelectionEvent e)
+                    {
+                        setEnabled(!hooksList.isSelectionEmpty()); // dependent from selection
+                    }
+                });
+            }
+
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                Hook selectedHook = (Hook) hooksList.getSelectedValue();
+                if (selectedHook != null && JOptionPane.showConfirmDialog(ItemEditor.this, "Вы действительно хотите удалить выбранный перехват?",
+                        "Удаление перехвата", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION)
+                {
+                    hooksClone.remove(selectedHook);
+                }
+            }
+        });
 
         // set common variables
         this.story = story;
@@ -138,7 +147,8 @@ public class ItemEditor extends AbstractEditor<Item>
         nameText.setText(item.getName());
         descText.setText(item.getDescription());
         // name and id generation listeners
-        nameText.getDocument().addDocumentListener(new DocumentListener(){
+        nameText.getDocument().addDocumentListener(new DocumentListener()
+        {
             @Override
             public void insertUpdate(DocumentEvent e)
             {
@@ -152,14 +162,15 @@ public class ItemEditor extends AbstractEditor<Item>
             }
 
             @Override
-            public void changedUpdate(DocumentEvent e) { /* do nothing */ }
+            public void changedUpdate(DocumentEvent e)
+            { /* do nothing */ }
         });
         idText.addKeyListener(new KeyAdapter()
         {
             @Override
             public void keyTyped(KeyEvent e)
             {
-                if(e.getKeyChar() != '\0')
+                if (e.getKeyChar() != '\0')
                 {
                     toGenerateId = false;
                 }
@@ -181,7 +192,7 @@ public class ItemEditor extends AbstractEditor<Item>
             @Override
             public void stateChanged(ChangeEvent e)
             {
-                wordsLabel.setText(((WordLinks)e.getSource()).getAllWords());
+                wordsLabel.setText(((WordLinks) e.getSource()).getAllWords());
             }
         });
 
@@ -194,7 +205,7 @@ public class ItemEditor extends AbstractEditor<Item>
         selectionModel.setValueIsAdjusting(true);
         try
         {
-            for(Location startLocation : item.getStartingPosition().getLocations())
+            for (Location startLocation : item.getStartingPosition().getLocations())
             {
                 int index = story.getLocations().indexOf(startLocation);
                 selectionModel.addSelectionInterval(index, index);
@@ -202,14 +213,14 @@ public class ItemEditor extends AbstractEditor<Item>
         }
         finally
         {
-           selectionModel.setValueIsAdjusting(false);
+            selectionModel.setValueIsAdjusting(false);
         }
         itemInLocationsList.ensureIndexIsVisible(selectionModel.getAnchorSelectionIndex());
 
         // set attributes
         attributesClone = GlazedLists.eventList(item.getAttributes());
         attributesList.setModel(new DefaultEventListModel<Attribute>(attributesClone));
-        
+
         // set hooks
         hooksClone = GlazedLists.eventList(item.hooks);
         hooksList.addMouseListener(new MouseAdapter()
@@ -217,7 +228,7 @@ public class ItemEditor extends AbstractEditor<Item>
             @Override
             public void mouseClicked(MouseEvent e)
             {
-                if(e.getClickCount() == 2)
+                if (e.getClickCount() == 2)
                 {
                     Hook selectedHook = (Hook) hooksList.getSelectedValue();
                     if (selectedHook != null)
@@ -228,21 +239,18 @@ public class ItemEditor extends AbstractEditor<Item>
             }
         });
         hooksList.setModel(new DefaultEventListModel<Hook>(hooksClone));
-
-        // initially update form actions
-        UpdateHookActions();
     }
 
     private boolean editHook(Hook hook)
     {
         try
         {
-        HookEditor hookEditor = new HookEditor(ItemEditor.this, hook, story.getAllActions());
-        if(hookEditor.showDialog())
-        {
-            hookEditor.getData(hook);
-            return true;
-        }
+            HookEditor hookEditor = new HookEditor(ItemEditor.this, hook, story.getAllActions());
+            if (hookEditor.showDialog())
+            {
+                hookEditor.getData(hook);
+                return true;
+            }
         }
         catch (IFML2Exception e)
         {
@@ -251,11 +259,21 @@ public class ItemEditor extends AbstractEditor<Item>
         return false;
     }
 
-    private void UpdateHookActions()
+    private void updateId()
     {
-        boolean hookIsSelected = hooksList.getSelectedValue() != null;
-        editHookAction.setEnabled(hookIsSelected);
-        deleteHookAction.setEnabled(hookIsSelected);
+        if (toGenerateId)
+        {
+            idText.setText(story.generateIdByName(nameText.getText(), Item.class));
+        }
+    }
+
+    @Override
+    protected void validateData() throws DataNotValidException
+    {
+        if (wordLinksClone.getWords().size() == 0)
+        {
+            throw new DataNotValidException("У предмета не задан словарь.", editWordsButton);
+        }
     }
 
     @Override
@@ -270,20 +288,12 @@ public class ItemEditor extends AbstractEditor<Item>
         item.getStartingPosition().setInventory(itemInInventoryCheck.isSelected());
         EventList<Location> locations = item.getStartingPosition().getLocations();
         locations.clear();
-        for(Object object : itemInLocationsList.getSelectedValues())
+        for (Object object : itemInLocationsList.getSelectedValues())
         {
             locations.add((Location) object);
         }
 
         item.setAttributes(attributesClone);
         item.hooks = hooksClone; // rewrite data in EventList
-    }
-
-    private void updateId()
-    {
-        if(toGenerateId)
-        {
-            idText.setText(story.generateIdByName(nameText.getText(), Item.class));
-        }
     }
 }
