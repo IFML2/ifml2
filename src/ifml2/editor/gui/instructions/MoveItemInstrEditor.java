c63267f20c31d2f2bab138633af93c40c1261f17
realsonic <realsonic3@gmail.com>
2013-10-15 21:51:58 +0400

    ifml2-214649 Редактирование инструкции "Перейти в локацию"

--- src/ifml2/editor/gui/instructions/MoveItemInstrEditor.java
+++ src/ifml2/editor/gui/instructions/MoveItemInstrEditor.java
@@ -1,6 +1,7 @@
 package ifml2.editor.gui.instructions;
 
 import ifml2.editor.IFML2EditorException;
+import ifml2.om.Story;
 import ifml2.vm.instructions.Instruction;
 import ifml2.vm.instructions.MoveItemInstruction;
 import org.jetbrains.annotations.NotNull;
@@ -16,8 +17,11 @@ public class MoveItemInstrEditor extends AbstractInstrEditor
     private JButton buttonCancel;
     private JTextField itemExprText;
     private JTextField toCollectionExprText;
+    private JRadioButton itemRadio;
+    private JRadioButton itemExprRadio;
+    private JComboBox itemCombo;
 
-    public MoveItemInstrEditor(Window owner, MoveItemInstruction instruction)
+    public MoveItemInstrEditor(Window owner, MoveItemInstruction instruction, Story.DataHelper storyDataHelper)
     {
         super(owner);
         initializeEditor(MOVE_ITEM_EDITOR_TITLE, contentPane, buttonOK, buttonCancel);
