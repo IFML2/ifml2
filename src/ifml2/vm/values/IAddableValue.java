3392718c9d83b34c96f79c41afc4078d78396a29
realsonic <realsonic3@gmail.com>
2016-02-21 22:59:29 +0300

    [Движок] Поддержка операции вычитания ("-") в выражениях.

--- src/ifml2/vm/values/IAddableValue.java
+++ src/ifml2/vm/values/IAddableValue.java
@@ -2,7 +2,6 @@ package ifml2.vm.values;
 
 import ifml2.vm.IFML2ExpressionException;
 
-public interface IAddableValue
-{
-    public abstract Value add(Value rightValue) throws IFML2ExpressionException;
+public interface IAddableValue {
+    Value add(Value rightValue) throws IFML2ExpressionException;
 }
