ca33cfd1075b0bef61cdd2412f2de60ca1935686
realsonic <realsonic3@gmail.com>
2016-08-14 15:25:50 +0300

    Рефакторинг AdaptedLocation #ifml2-334876

--- src/ifml2/om/xml/xmladapters/LocationAdapter.java
+++ src/ifml2/om/xml/xmladapters/LocationAdapter.java
@@ -5,8 +5,6 @@ import org.jetbrains.annotations.NotNull;
 
 import javax.xml.bind.annotation.*;
 import javax.xml.bind.annotation.adapters.XmlAdapter;
-import java.util.HashMap;
-import java.util.function.Supplier;
 
 import static ifml2.om.Location.ExitDirection.*;
 
@@ -21,42 +19,9 @@ public class LocationAdapter extends XmlAdapter<LocationAdapter.AdaptedLocation,
         return location instanceof AdaptedLocation ? (AdaptedLocation) location : new AdaptedLocation(location);
     }
 
-    // TODO: 14.08.2016 try to move to get and set using super and @XmlElement above getNorth and setNorth and so on
-    
     @XmlAccessorType(XmlAccessType.NONE)
     @XmlType(name = "location")
     public static class AdaptedLocation extends Location implements Cloneable {
-        @XmlElement(name = "north")
-        @XmlIDREF
-        private Location north;
-        @XmlElement(name = "north-east")
-        @XmlIDREF
-        private Location northEast;
-        @XmlElement(name = "east")
-        @XmlIDREF
-        private Location east;
-        @XmlElement(name = "south-east")
-        @XmlIDREF
-        private Location southEast;
-        @XmlElement(name = "south")
-        @XmlIDREF
-        private Location south;
-        @XmlElement(name = "south-west")
-        @XmlIDREF
-        private Location southWest;
-        @XmlElement(name = "west")
-        @XmlIDREF
-        private Location west;
-        @XmlElement(name = "north-west")
-        @XmlIDREF
-        private Location northWest;
-        @XmlElement(name = "up")
-        @XmlIDREF
-        private Location up;
-        @XmlElement(name = "down")
-        @XmlIDREF
-        private Location down;
-
         /**
          * Constructor needed for JAXB
          */
@@ -65,93 +30,106 @@ public class LocationAdapter extends XmlAdapter<LocationAdapter.AdaptedLocation,
 
         public AdaptedLocation(@NotNull Location location) throws CloneNotSupportedException {
             location.copyTo(this);
-            north = location.getExit(NORTH);
-            northEast = location.getExit(NORTH_EAST);
-            east = location.getExit(EAST);
-            southEast = location.getExit(SOUTH_EAST);
-            south = location.getExit(SOUTH);
-            southWest = location.getExit(SOUTH_WEST);
-            west = location.getExit(WEST);
-            northWest = location.getExit(NORTH_WEST);
-            up = location.getExit(UP);
-            down = location.getExit(DOWN);
         }
 
-        HashMap<ExitDirection, Supplier<Location>> exitsMap = new HashMap<ExitDirection, Supplier<Location>>() {
-            {
-                put(NORTH, () -> north);  // FIXME: 14.08.2016 add others
-            }
-        };
-
-        /**
-         * Override getExit() to yield exits from plain fields to HashMap
-         */
-        @Override
-        public Location getExit(ExitDirection exitDirection) {
-            switch (exitDirection) {
-                case NORTH:
-                    return north;
-                case NORTH_EAST:
-                    return northEast;
-                case EAST:
-                    return east;
-                case SOUTH_EAST:
-                    return southEast;
-                case SOUTH:
-                    return south;
-                case SOUTH_WEST:
-                    return southWest;
-                case WEST:
-                    return west;
-                case NORTH_WEST:
-                    return northWest;
-                case UP:
-                    return up;
-                case DOWN:
-                    return down;
-                default:
-                    throw new IllegalArgumentException(String.format("Unknown ExitDirection = %s", exitDirection));
-            }
+        @XmlElement(name = "north")
+        @XmlIDREF
+        private Location getNorth() {
+            return getExit(NORTH);
         }
 
-        /**
-         * Override setExit() to set exits to plain fields from HashMap
-         */
-        @Override
-        public void setExit(ExitDirection exitDirection, Location location) {
-            switch (exitDirection) {
-                case NORTH:
-                    north = location;
-                    break;
-                case NORTH_EAST:
-                    northEast = location;
-                    break;
-                case EAST:
-                    east = location;
-                    break;
-                case SOUTH_EAST:
-                    southEast = location;
-                    break;
-                case SOUTH:
-                    south = location;
-                    break;
-                case SOUTH_WEST:
-                    southWest = location;
-                    break;
-                case WEST:
-                    west = location;
-                    break;
-                case NORTH_WEST:
-                    northWest = location;
-                    break;
-                case UP:
-                    up = location;
-                    break;
-                case DOWN:
-                    down = location;
-                    break;
-            }
+        private void setNorth(Location north) {
+            setExit(NORTH, north);
         }
 
+        @XmlElement(name = "north-east")
+        @XmlIDREF
+        public Location getNorthEast() {
+            return getExit(NORTH_EAST);
+        }
+
+        void setNorthEast(Location northEast) {
+            setExit(NORTH_EAST, northEast);
+        }
+
+        @XmlElement(name = "east")
+        @XmlIDREF
+        public Location getEast() {
+            return getExit(EAST);
+        }
+
+        public void setEast(Location east) {
+            setExit(EAST, east);
+        }
+
+        @XmlElement(name = "south-east")
+        @XmlIDREF
+        public Location getSouthEast() {
+            return getExit(SOUTH_EAST);
+        }
+
+        public void setSouthEast(Location southEast) {
+            setExit(SOUTH_EAST, southEast);
+        }
+
+        @XmlElement(name = "south")
+        @XmlIDREF
+        public Location getSouth() {
+            return getExit(SOUTH);
+        }
+
+        public void setSouth(Location south) {
+            setExit(SOUTH, south);
+        }
+
+        @XmlElement(name = "south-west")
+        @XmlIDREF
+        public Location getSouthWest() {
+            return getExit(SOUTH_WEST);
+        }
+
+        public void setSouthWest(Location southWest) {
+            setExit(SOUTH_WEST, southWest);
+        }
+
+        @XmlElement(name = "west")
+        @XmlIDREF
+        public Location getWest() {
+            return getExit(WEST);
+        }
+
+        public void setWest(Location west) {
+            setExit(WEST, west);
+        }
+
+        @XmlElement(name = "north-west")
+        @XmlIDREF
+        public Location getNorthWest() {
+            return getExit(NORTH_WEST);
+        }
+
+        public void setNorthWest(Location northWest) {
+            setExit(NORTH_WEST, northWest);
+        }
+
+        @XmlElement(name = "up")
+        @XmlIDREF
+        public Location getUp() {
+            return getExit(UP);
+        }
+
+        public void setUp(Location up) {
+            setExit(UP, up);
+        }
+
+        @XmlElement(name = "down")
+        @XmlIDREF
+        public Location getDown() {
+            return getExit(DOWN);
+        }
+
+        public void setDown(Location down) {
+            setExit(DOWN, down);
+        }
     }
 }
