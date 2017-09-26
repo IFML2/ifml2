769ac8ded052132b453704478c9bf8410a29beb6
Dmitry Eliseev <eliseevsan@gmail.com>
2017-09-26 12:44:50 +0200

    Add Grammar.kt

---
+++ grammar/src/main/kotlin/ifml2/grammar/Grammar.kt
@@ -0,0 +1,13 @@
+package ifml2.grammar
+
+import org.demo.grammar.word.Word
+
+object Grammar {
+    fun parseWord(word: String): List<Word> {
+        TODO("TBD")
+    }
+
+    fun parse(sentence: String): List<List<Word>> {
+        TODO("TBD")
+    }
+}
