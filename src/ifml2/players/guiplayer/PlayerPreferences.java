eb5e94ff4e100e8e2b41468ce43b539aaac5a1d7
realsonic <realsonic3@gmail.com>
2018-10-01 11:55:26 +0300

    ifml2-33 Плеер.ехе цвета шрифты (исправлены имена настроек в реестре)

--- src/ifml2/players/guiplayer/PlayerPreferences.java
+++ src/ifml2/players/guiplayer/PlayerPreferences.java
@@ -3,8 +3,8 @@ package ifml2.players.guiplayer;
 import java.util.prefs.Preferences;
 
 class PlayerPreferences {
-    private static final String PLAYER_PREFS_ROOT = "/IFML2/Player";
-    private static final String THEME_KEY = "ThemeName";
+    private static final String PLAYER_PREFS_ROOT = "/ifml2/player";
+    private static final String THEME_KEY = "themename";
     private static Preferences _preferences = Preferences.userRoot().node(PLAYER_PREFS_ROOT);
 
     static String getPlayerThemeName() {
